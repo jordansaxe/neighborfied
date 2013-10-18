69e87a9efe0b60e058e1e72e9c5db9fe6c21587a
Jordan Saxe <jordansaxe15@gmail.com>
2013-10-18 14:58:47 -0400

    Added jquery masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require turbolinks
 //= require_tree .
+//= require masonry/jquery.masonry
+
 
 // Loads all Bootstrap javascripts
 //= require bootstrap
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Neighborfied::Application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # This sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
